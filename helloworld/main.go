b2e58e2daa2a04c56fe8c8fffc22b85d5348f4c4
zyp <zhou.youpeng@foxmail.com>
2022-06-23 12:42:31 +0800

    feat: commit

--- helloworld/main.go
+++ helloworld/main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	fmt.Println("hello, world")
+
+	// define variable
+	// style 1
+	var age int = 10
+	var name string = "张三"
+	var str string = "hello, world"
+
+	// style 2
+	var weigth = 100
+
+	// style 3
+	height := 180
+
+	// variable must be used
+	fmt.Println(age, name, str, weigth, height)
+
+	// define const
+	// const dont use
+	const PI = 3.1415926
+	const ONE int = 1
 }
